21cb09e14f0df27aa8e4dcfe64edb3d76854a99b
fstrijth <fien.strijthaegen@gmail.com>
2023-04-19 17:46:42 +0200

    Heat maps (nog niet bruikbaar)

--- diff_gene_expression.R
+++ diff_gene_expression.R
@@ -1,24 +1,36 @@
 library("edgeR")
 
-#Completely followed this:
-#https://www.reneshbedre.com/blog/edger-tutorial.html
-
-#Read file
+#https://bioinfo-dirty-jobs.github.io/rana2/lectures/08.rnaseq_edger/
 rna_counts = read.delim(file='RNA-read-counts.tsv',sep='\t',header=TRUE,row.names=1)
-#Remove description column
 rna_counts = subset(rna_counts, select=-c(Description))
-#Get column names
-sample_info = colnames(rna_counts)
-#Create DGEList data class
-dge = DGEList(counts = rna_counts, group = factor(sample_info))
-#Filter out genes with low counts
-keep = filterByExpr(y = dge)
-dge = dge[keep, ,keep.lib.sizes=FALSE]
-#Normalization and effective library sizes
-dge = calcNormFactors(object=dge)
-#Model fitting and estimating dispersions
-#dge = estimateDisp(y = dge) FOR MULTIPLE BATCHES?
+cpm_log_rna = cpm(rna_counts, log=TRUE)
+heatmap(cor(cpm_log_rna))
 
-#Testing for differential gene expression
-et = exactTest(object=dge)
 
+morph_counts = read.delim(file='morphological-counts.tsv', sep='\t', header=TRUE, row.names=1)
+cpm_log_morph = cpm(morph_counts, log=TRUE)
+heatmap(cor(cpm_log_morph))
+
+# 
+# #Completely followed this:
+# #https://www.reneshbedre.com/blog/edger-tutorial.html
+# 
+# #Read file
+# rna_counts = read.delim(file='RNA-read-counts.tsv',sep='\t',header=TRUE,row.names=1)
+# #Remove description column
+# rna_counts = subset(rna_counts, select=-c(Description))
+# #Get column names
+# sample_info = colnames(rna_counts)
+# #Create DGEList data class
+# dge = DGEList(counts = rna_counts, group = factor(sample_info))
+# #Filter out genes with low counts
+# keep = filterByExpr(y = dge)
+# dge = dge[keep, ,keep.lib.sizes=FALSE]
+# #Normalization and effective library sizes
+# dge = calcNormFactors(object=dge)
+# #Model fitting and estimating dispersions
+# #dge = estimateDisp(y = dge) FOR MULTIPLE BATCHES?
+# 
+# #Testing for differential gene expression
+# et = exactTest(object=dge)
+# 
