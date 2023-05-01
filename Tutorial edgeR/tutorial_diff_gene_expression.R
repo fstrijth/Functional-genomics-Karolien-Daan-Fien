d81893b30981abbcceb9d3f9289de3ec0f8e797a
fstrijth <fien.strijthaegen@gmail.com>
2023-05-01 16:09:33 +0200

    Exploration of morphological cluster dataset

--- Tutorial edgeR/tutorial_diff_gene_expression.R
+++ Tutorial edgeR/tutorial_diff_gene_expression.R
@@ -1,6 +1,7 @@
 library("edgeR")
 library(stringr)
 library("xlsx")
+library("sjmisc")
 
 # Analysis based on this tutorial:
 # https://bioinfo-dirty-jobs.github.io/rana2/lectures/08.rnaseq_edger/
@@ -20,23 +21,14 @@ morph_counts = read.delim(file='morphological-counts.tsv',sep='\t',header=TRUE,r
 
 #PCA on unfiltered data
 cpm_log_rna = cpm(rna_counts, log=TRUE)
-pca_unclean = prcomp(t(cpm_log_rna),scale.=TRUE)
-#summary(pca_unclean)
-#plot(pca_unclean)
-
-#There seem to be two groups: one with PC1 > 0 and one with PC1 < 0
-colors = rep("black", length=nrow(cpm_log_rna))
-colors[pca_unclean$x[,1] > 0] = "red"
-colors[pca_unclean$x[,1] < 0] = "blue"
-plot(pca_unclean$x[, 1], pca_unclean$x[, 2], pch = "*", xlab = "PC1", ylab = "PC2", col=colors)
-#text(pca_unclean$x[, 1], pca_unclean$x[, 2], labels = colnames(cpm_log_rna))
-
-
+pca = prcomp(t(cpm_log_rna),scale.=TRUE)
+summary(pca)
+#plot(pca)
 
 #Group the clinical data in terms of these two groups
 groups = rep(0, length=nrow(clin_data))
 for (i in 1:ncol(rna_counts)) {
-  if (pca_unclean$x[,1][i] > 0){
+  if (pca$x[,1][i] > 0){
     groups[i] = 1
   } else{
       groups[i] = 2
@@ -44,29 +36,31 @@ for (i in 1:ncol(rna_counts)) {
 }
 clin_data$expression_group = groups
 
+#Also add a column that is 1 if any keyword associated to thyroiditis is present
+thyroiditis = rep(0, length=nrow(clin_data))
+for (i in 1:nrow(clin_data)) {
+  if (str_contains(clin_data$SMPTHNTS[i], c("hashimoto", "thyroiditis", "goiter", "fibrosis", "goitre", "fibrous"), ignore.case=TRUE, logic="or")) {
+    thyroiditis[i] = 1
+  }
+}
+thyroiditis
+clin_data$thyroiditis = thyroiditis
+
+#There seem to be two groups: one with PC1 > 0 and one with PC1 < 0
+colors = rep("black", length=nrow(cpm_log_rna))
+colors[clin_data$AGE > 50] = "red"
+colors[clin_data$AGE < 50] = "blue"
+plot(pca$x[, 1], pca$x[, 2], pch = "*", xlab = "PC1", ylab = "PC2", col=colors)
+#text(pca$x[, 1], pca$x[, 2], labels = colnames(cpm_log_rna))
+
+
 #Write to file
 write.xlsx(clin_data, file = "clinical-data-with-expression-group.xlsx")
 
 
-# # BELOW I TRIED TO FILTER OUT THE LOWLY EXPRESSED GENES FIRST,
-# # BUT UNFILTERED LOOKS MORE INTERESTING
-# # Remove genes that are unexpressed or very lowly expressed in the samples
-# # Used same cutoff as tutorial: based on median log2-transformed counts
-# # per gene per million mapped reads (cpm in edgeR)
-# median_log2_cpm = apply(cpm_log_rna, 1, median)
-# expr_cutoff = -1
-# 
-# # This histogram shows our cutoff
-# hist(median_log2_cpm)
-# abline(v = expr_cutoff, col="red", lwd=3)
-# 
-# sum(median_log2_cpm > expr_cutoff)
-# cleaned_rna_counts = rna_counts[median_log2_cpm > expr_cutoff,]
-# cpm_log_cleaned = cpm(cleaned_rna_counts, log=TRUE)
-# heatmap(cor(cpm_log_cleaned))
-# 
-# #PCA on filtered data
-# pca = prcomp(t(cpm_log_cleaned),scale.=TRUE)
-# plot(pca$x[, 1], pca$x[, 2], pch = "*", xlab = "PC1", ylab = "PC2")
-# text(pca$x[, 1], pca$x[, 2], labels = colnames(cpm_log_cleaned))
-# summary(pca)
+#PCA on morphological data
+cpm_log_morph = cpm(morph_counts, log=TRUE)
+pca = prcomp(t(cpm_log_morph),scale.=TRUE)
+#summary(pca)
+plot(pca$x[, 1], pca$x[, 2], pch = "*", xlab = "PC1", ylab = "PC2")
+
--- exploration_morphology.R
+++ exploration_morphology.R
@@ -2,9 +2,13 @@ library("DESeq2")
 library(ggplot2)
 
 # Q2: clinical data vs. morphology
-# 1) Compute systematically associations between clinical variables and morphological cluster counts. The purpose is to compare the magnitude of the associations of the different variables with morphology.
+# 1) Compute systematically associations between clinical variables and morphological
+#    cluster counts. The purpose is to compare the magnitude of the associations of the 
+#    different variables with morphology.
 # 2) Discuss the association with technical variables.
-# 3) For non-technical variables, redo the analysis with adjustment for the confounding technical variables, if any is reported in Q2.2. Report and discuss significant associations.
+# 3) For non-technical variables, redo the analysis with adjustment for the confounding 
+#    technical variables, if any is reported in Q2.2. Report and discuss significant 
+#    associations.
 
 
 # Inspired by this tutorial:
@@ -13,31 +17,69 @@ library(ggplot2)
 
 #load data
 clinical_data = read.delim(file='clinical-data.tsv',sep ='\t',header=TRUE,row.names=1)
-clin_categories = clinical_data[c('COHORT', 'DTHHRDY')]
-clin_categories$GTEXID = row.names(clin_categories)
-rownames(clin_categories) = c()
+#DTHHRDY is a categorical variable, set its type to character to that it can be viewed as such by deseq2
+clinical_data$DTHHRDY <- as.character(clinical_data$DTHHRDY)
 morph_counts = t(read.delim(file='morphological-counts.tsv', sep='\t', header=TRUE, row.names=1))
 row.names(morph_counts) = gsub("Mophological.cluster.", "", row.names(morph_counts))
 
 
-dds = DESeqDataSetFromMatrix(countData = morph_counts, 
-                             colData = clin_categories,
-                             design = ~COHORT)
-dds = DESeq(dds)
-res = results(dds)
-head(results(dds, tidy=TRUE))
-summary(res)
-res <- res[order(res$padj),]
-head(res)
+#Automatically generates interesting plots in function of var
+#All plots are saved under morph_plots/ and file names end in the variable name
+#In case of a categorical variable, deseq2's plotCounts can be called, if you want that
+#, set pltcnt to TRUE
+morph_analysis <- function(var, formula, pltcnt) {
+  #construct DESEQDataset object
+  dds = DESeqDataSetFromMatrix(countData = morph_counts, 
+                               colData = clinical_data,
+                               design = formula)
+  #Run DESeq function
+  dds = DESeq(dds)
+  res = results(dds)
+  #summary of the differential morphology expression
+  summary(res)
+  
+  #Plotting adjusted p-values for each morphological cluster
+  png(paste("morph_plots/adjusted_P_values_",var,".png", sep=""))
+  plot(rownames(morph_counts), res$padj)
+  abline(h=0.1, col="red") #cutoff?
+  dev.off()
+  
+  #Normalized counts of cluster
+  png(paste("morph_plots/normalized_counts_",var,".png", sep=""))
+  plotMA(res)
+  dev.off()
+  
+  #sort results by p-value
+  res <- res[order(res$padj),]
 
-
-par(mfrow=c(2,3))
-clusters = c("57", "33", "48", "12", "35", "5")
-for (cluster in clusters) {
-  plotCounts(dds, gene=cluster, intgroup='COHORT')
+  
+  #Plot the counts of the clusters with the lowest adjusted p-values
+  if (pltcnt) {
+    png(paste("morph_plots/counts_by_",var,".png", sep=""))
+    par(mfrow=c(3,4))
+    clusters = rownames(head(res,10))
+    for (cluster in clusters) {
+      plotCounts(dds, gene=cluster, intgroup=var)
+    }
+    dev.off()
+  }
+  
+  #Volcano plot
+  png(paste("morph_plots/volcano_",var,".png", sep=""))
+  par(mfrow=c(1,1))
+  with(res, plot(log2FoldChange, -log10(pvalue), pch=20, main="Volcano plot", xlim=c(-3,3)))
+  with(subset(res, padj<.01 ), points(log2FoldChange, -log10(pvalue), pch=20, col="blue"))
+  with(subset(res, padj<.01 & abs(log2FoldChange)>2), points(log2FoldChange, -log10(pvalue), pch=20, col="red"))
+  dev.off()
+  
+  #PCA
+  #TODO???
 }
 
-par(mfrow=c(1,1))
-with(res, plot(log2FoldChange, -log10(pvalue), pch=20, main="Volcano plot", xlim=c(-3,3)))
-with(subset(res, padj<.01 ), points(log2FoldChange, -log10(pvalue), pch=20, col="blue"))
-with(subset(res, padj<.01 & abs(log2FoldChange)>2), points(log2FoldChange, -log10(pvalue), pch=20, col="red"))
+morph_analysis("COHORT", ~ COHORT, TRUE)
+morph_analysis("DTHHRDY", ~ DTHHRDY, TRUE)
+morph_analysis("AGE", ~ AGE, FALSE)
+morph_analysis("HGHT", ~ HGHT, FALSE)
+morph_analysis("WGHT", ~ WGHT, FALSE)
+
+
